3d97f5bcca3d3f240660237ce2010d52599fe07f
Fatimah Az Zahra <140265471+ftmhazzahra@users.noreply.github.com>
2024-06-02 14:32:07 +0700

    Add files via upload

---
+++ Modul 1/Guided1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+// Fatimah Az Zahra/2211102160/IF-10-D
+using namespace std;
+int main()
+{
+    cout << " Baris ini tanpa end1.";
+    cout << "Manipulator end1" << endl;
+    cout << "Manipulator ends" << ends;
+    cout << dec << 10 << endl;
+    cout << hex << 10 << endl;
+    cout << oct << 10 << endl;
+    cout << setiosflags(ios::fixed);
+    cout << setprecision(4) << 123.4567891 << endl;
+    return 0;
+}
